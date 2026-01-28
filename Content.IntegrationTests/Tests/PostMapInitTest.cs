573a81e99dc0af0004d8d53306569b39ca27f1ec
Dunrab <dunrab@gmail.com>
2026-01-27 19:08:01 -0500

    requested changes

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -87,25 +87,25 @@ namespace Content.IntegrationTests.Tests
             "TestTeg",
             "CentComm",
             "MeteorArena",
-            "Academy", //DeltaV
+            //"Academy", //DeltaV
             "Arena", // DeltaV
-            "ArenaMedieval", //DeltaV
+            // "ArenaMedieval", //DeltaV
             "Asterisk", // DeltaV
-            "Byoin", // DeltaV
-            "Chibi", // DeltaV
-            "Division", // DeltaV
+            //"Byoin", // DeltaV
+            //"Chibi", // DeltaV
+            //"Division", // DeltaV
             "Edge", // DeltaV
-            "Elegance", // DeltaV
+            //"Elegance", // DeltaV
             "Glacier", // DeltaV
             "Hammurabi", // DeltaV
             "Lighthouse", // DeltaV
-            "Micro", // DeltaV
-            "Ovni", //DeltaV
+            //"Micro", // DeltaV
+            //"Ovni", //DeltaV
             "Pebble", // DeltaV
-            "PebbleHiring", // DeltaV
+            //"PebbleHiring", // DeltaV
             "Shoukou", // DeltaV
             "Submarine", //DeltaV
-            "Terra", //DeltaV
+            //"Terra", //DeltaV
             "TheHive", // DeltaV
             "Tortuga", // DeltaV
             "Amber", //Floofstation
