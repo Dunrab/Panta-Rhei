b4768ad006a5cae60b3df7d0b50059e1c2f3dd4c
Dunrab <dunrab@gmail.com>
2026-03-15 04:04:42 -0400

    fix attempt

--- Content.Shared/Entry/EntryPoint.cs
+++ Content.Shared/Entry/EntryPoint.cs
@@ -37,6 +37,7 @@ namespace Content.Shared.Entry
         public override void Init()
         {
             IgnorePrototypes();
+            _prototypeManager.RegisterIgnore("stationGoal"); // Floofstation
         }
 
         public override void PostInit()
