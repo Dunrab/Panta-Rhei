b17cca301462cd7e01e7e53acf7880236d722304
Dunrab <dunrab@gmail.com>
2026-03-15 02:38:07 -0400

    initial commit

--- Content.Shared/_EE/CCVars/CCVars.EE.cs
+++ Content.Shared/_EE/CCVars/CCVars.EE.cs
@@ -10,4 +10,25 @@ public sealed partial class EECVars
     /// </summary>
     public static readonly CVarDef<int> ChatStackLastLines =
         CVarDef.Create("chat.chatstack_last_lines", 1, CVar.CLIENTONLY | CVar.ARCHIVE, "How far into the chat history to look when looking for similiar messages to coalesce them.");
+
+    /// <summary>
+    ///     Whether height & width sliders adjust a player's bloodstream volume.
+    /// </summary>
+    /// <remarks>
+    ///     This can be configured more precisely by modifying BloodstreamAffectedByMassComponent.
+    /// </remarks>
+    public static readonly CVarDef<bool> HeightAdjustModifiesBloodstream =
+        CVarDef.Create("heightadjust.modifies_bloodstream", true, CVar.SERVERONLY);
+
+    /// <summary>
+    ///     Enables station goals
+    /// </summary>
+    public static readonly CVarDef<bool> StationGoalsEnabled =
+        CVarDef.Create("game.station_goals", true, CVar.SERVERONLY);
+
+    /// <summary>
+    ///     Chance for a station goal to be sent
+    /// </summary>
+    public static readonly CVarDef<float> StationGoalsChance =
+        CVarDef.Create("game.station_goals_chance", 0.1f, CVar.SERVERONLY);
 }
