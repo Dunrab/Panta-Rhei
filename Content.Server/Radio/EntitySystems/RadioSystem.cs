c20d6ab3ddf8a804e66d791eb7d1b0c8bd92a3f6
Dunrab <dunrab@gmail.com>
2026-04-20 11:19:40 -0400

    initial commits

--- Content.Server/Radio/EntitySystems/RadioSystem.cs
+++ Content.Server/Radio/EntitySystems/RadioSystem.cs
@@ -4,6 +4,7 @@ using Content.Server.Power.Components;
 using Content.Shared._Floof.Language;
 using Content.Shared.Chat;
 using Content.Shared.Database;
+using Content.Shared.Ghost; // Nuclear-14 - handheld radio
 using Content.Shared.Radio;
 using Content.Shared.Radio.Components;
 using Content.Shared.Speech;
@@ -52,6 +53,19 @@ public sealed partial class RadioSystem : EntitySystem // Floofstation - made pa
         }
     }
 
+    //Nuclear-14
+    /// <summary>
+    /// Gets the message frequency, if there is no such frequency, returns the standard channel frequency.
+    /// </summary>
+    public int GetFrequency(EntityUid source, RadioChannelPrototype channel)
+    {
+        if (TryComp<RadioMicrophoneComponent>(source, out var radioMicrophone))
+            return radioMicrophone.Frequency;
+
+        return channel.Frequency;
+    }
+
+
     private void OnIntrinsicReceive(EntityUid uid, IntrinsicRadioReceiverComponent component, ref RadioReceiveEvent argsRaw)
     {
         var args = ApplyLanguageUnderstanding(argsRaw, uid); // Floofstation - languages
@@ -62,18 +76,22 @@ public sealed partial class RadioSystem : EntitySystem // Floofstation - made pa
     /// <summary>
     /// Send radio message to all active radio listeners
     /// </summary>
-    public void SendRadioMessage(EntityUid messageSource, string message, ProtoId<RadioChannelPrototype> channel, EntityUid radioSource, bool escapeMarkup = true)
+    public void SendRadioMessage(EntityUid messageSource, string message, ProtoId<RadioChannelPrototype> channel, EntityUid radioSource, bool escapeMarkup = true, int? frequency = null) // Nuclear-14 - handheld radio - added frequency
     {
-        SendRadioMessage(messageSource, message, _prototype.Index(channel), radioSource, escapeMarkup: escapeMarkup);
+        SendRadioMessage(messageSource, message, _prototype.Index(channel), radioSource, escapeMarkup: escapeMarkup, frequency: frequency); // Nuclear-14 - handheld radio - added frequency
     }
 
     /// <summary>
     /// Send radio message to all active radio listeners
     /// </summary>
     /// <param name="messageSource">Entity that spoke the message</param>
+    /// <param name="message"></param>
+    /// <param name="channel"></param>
     /// <param name="radioSource">Entity that picked up the message and will send it, e.g. headset</param>
+    /// <param name="frequency"></param>
+    /// <param name="escapeMarkup"></param>
     /// <param name="languageOverride">Added by floofstation - allows overriding the language of the message. Defaults to the language of the radio source.</param>
-    public void SendRadioMessage(EntityUid messageSource, string message, RadioChannelPrototype channel, EntityUid radioSource, bool escapeMarkup = true, LanguagePrototype? languageOverride = null)
+    public void SendRadioMessage(EntityUid messageSource, string message, RadioChannelPrototype channel, EntityUid radioSource, bool escapeMarkup = true, int? frequency = null, LanguagePrototype? languageOverride = null)
     {
         // TODO if radios ever garble / modify messages, feedback-prevention needs to be handled better than this.
         if (!_messages.Add(message))
@@ -135,6 +153,9 @@ public sealed partial class RadioSystem : EntitySystem // Floofstation - made pa
         var sourceServerExempt = _exemptQuery.HasComp(radioSource);
 
         var radioQuery = EntityQueryEnumerator<ActiveRadioComponent, TransformComponent>();
+        if (frequency == null) // Nuclear-14
+            frequency = GetFrequency(messageSource, channel); // Nuclear-14
+
         while (canSend && radioQuery.MoveNext(out var receiver, out var radio, out var transform))
         {
             if (!radio.ReceiveAllChannels)
@@ -143,6 +164,9 @@ public sealed partial class RadioSystem : EntitySystem // Floofstation - made pa
                                                              !intercom.SupportedChannels.Contains(channel.ID)))
                     continue;
             }
+
+            if (!HasComp<GhostComponent>(receiver) && GetFrequency(receiver, channel) != frequency) // Nuclear-14 - handheld radio - added frequency check
+                continue;
 
             if (!channel.LongRange && transform.MapID != sourceMapId && !radio.GlobalReceive)
                 continue;
