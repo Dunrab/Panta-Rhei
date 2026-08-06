c5a77e708830c2b2cddec8c57e72a894bc632b59
Dunrab <dunrab@gmail.com>
2026-08-06 13:07:05 -0400

    update disclaimer

--- Content.Server/_Floof/Info/NsfwDisclaimerSystem.cs
+++ Content.Server/_Floof/Info/NsfwDisclaimerSystem.cs
@@ -4,16 +4,12 @@ using Content.Shared.FloofStation.Info;
 using Robust.Shared.Configuration;
 using Robust.Shared.Network;
 
-
 namespace Content.Server._Floof.Info;
-
 
 public sealed class NsfwDisclaimerSystem : EntitySystem
 {
     [Dependency] private readonly INetManager _netManager = default!;
     [Dependency] private readonly IConfigurationManager _cfg = default!;
-
-    private static DateTime LastValidReadTime => DateTime.UtcNow - TimeSpan.FromDays(60);
 
     public override void Initialize()
     {
