2c96429acacbaf7f12d188115cf14fa03f2c380b
Dunrab <dunrab@gmail.com>
2026-08-10 23:00:21 -0400

    fishy trait

---
+++ Content.Server/_Floof/Nosebleed/NosebleedSystem.cs
@@ -0,0 +1,83 @@
+﻿using Content.Server.Body.Systems;
+using Content.Shared._Floof.Nosebleed;
+using Content.Shared.Mobs.Components;
+using Content.Shared.Mobs.Systems;
+using Content.Shared.Popups;
+using Robust.Shared.Random;
+using Robust.Shared.Timing;
+
+namespace Content.Server._Floof.Nosebleed;
+
+public sealed class NosebleedSystem : EntitySystem
+{
+    [Dependency] private readonly BloodstreamSystem _bloodstream = default!;
+    [Dependency] private readonly IGameTiming _timing = default!;
+    [Dependency] private readonly MobStateSystem _mobState = default!;
+    [Dependency] private readonly IRobustRandom _random = default!;
+    [Dependency] private readonly SharedPopupSystem _popup = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<NosebleedComponent, ComponentStartup>(OnStartup);
+    }
+
+    // start her up
+    private void OnStartup(EntityUid uid, NosebleedComponent comp, ComponentStartup args)
+    {
+        ScheduleNextNosebleed(comp);
+    }
+
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        // whos got our comp
+        var query = EntityQueryEnumerator<NosebleedComponent>();
+
+        // schedule the nosebleed or wait
+        while (query.MoveNext(out var uid, out var comp))
+        {
+            if (comp is not { NextNosebleed: { } nextNosebleed })
+            {
+                ScheduleNextNosebleed(comp);
+                continue;
+            }
+
+            // keep going if its not time to schedule another nosebleed
+            if (_timing.CurTime < nextNosebleed)
+                continue;
+
+            CauseNosebleed(uid, comp);
+        }
+    }
+
+    private void ScheduleNextNosebleed(NosebleedComponent comp)
+    {
+        // our min, max times from our nosebleed comp
+        var delay = _random.Next(TimeSpan.FromSeconds(comp.MinimumDelay), TimeSpan.FromSeconds(comp.MaximumDelay));
+
+        // the current time + our delay
+        comp.NextNosebleed = _timing.CurTime + delay;
+    }
+
+    private void CauseNosebleed(EntityUid uid, NosebleedComponent comp)
+    {
+        // we just had a nosebleed so we need to schedule the next one
+        ScheduleNextNosebleed(comp);
+
+        // get our mobstate for below
+        if (!TryComp<MobStateComponent>(uid, out var mobState))
+            return;
+
+        // are they not alive? it would be funny if we let it happen on the dead...
+        if (!_mobState.IsAlive(uid, mobState))
+            return;
+
+        // send a red popup with our nosebleed message (same look as the pysonic nosebleed event)
+        _popup.PopupEntity(Loc.GetString("nosebleed-message"), uid, uid, PopupType.MediumCaution);
+
+        // bleed on the floor time (the poor janitors im sorry)
+        _bloodstream.TryModifyBleedAmount(uid, comp.BleedAmount);
+    }
+}
---
+++ Content.Shared/_Floof/Nosebleed/NosebleedComponent.cs
@@ -0,0 +1,29 @@
+﻿namespace Content.Shared._Floof.Nosebleed;
+
+[RegisterComponent]
+public sealed partial class NosebleedComponent : Component
+{
+    /// <summary>
+    /// Blood that will be lost, same as the noospheric nosebleed event
+    /// </summary>
+    [DataField("bleedAmount"), ViewVariables(VVAccess.ReadWrite)]
+    public float BleedAmount = 2.5f;
+
+    /// <summary>
+    /// Minimum delay between nosebleeds in seconds, default is 900 seconds or 15 minutes
+    /// </summary>
+    [DataField("minDelay"), ViewVariables(VVAccess.ReadWrite)]
+    public float MinimumDelay = 900f;
+
+    /// <summary>
+    /// Maximum delay between nosebleeds in seconds, default is 3600 seconds or 60 minutes
+    /// </summary>
+    [DataField("maxDelay"), ViewVariables(VVAccess.ReadWrite)]
+    public float MaximumDelay = 3600;
+
+    /// <summary>
+    /// When our next nosebleed will happen
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite)]
+    public TimeSpan NextNosebleed;
+}
