8abe99900544a90e38b53e1a4e45bab3b46620e1
Dunrab <dunrab@gmail.com>
2026-08-20 16:07:52 -0400

    fishy

---
+++ Content.Server/_Floof/SelfInject/SelfInjectSystem.cs
@@ -0,0 +1,46 @@
+﻿using Content.Shared._Floof.SelfInject.Component;
+using Content.Shared.Actions;
+using Content.Shared.Body.Components;
+using Content.Shared.Chemistry.EntitySystems;
+using Robust.Shared.Audio.Systems;
+
+namespace Content.Server._Floof.SelfInject;
+
+public sealed class SelfInjectSystem : EntitySystem
+{
+    [Dependency] private readonly SharedActionsSystem _actions = default!;
+    [Dependency] private readonly SharedSolutionContainerSystem _solution = default!;
+    [Dependency] private readonly SharedAudioSystem _audio = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<SelfInjectComponent, MapInitEvent>(OnMapInit);
+        SubscribeLocalEvent<SelfInjectComponent, ActionSelfInjectEvent>(OnInject);
+    }
+
+    private void OnMapInit(Entity<SelfInjectComponent> ent, ref MapInitEvent args)
+    {
+        if (string.IsNullOrEmpty(ent.Comp.Action))
+            return;
+
+        _actions.AddAction(ent, ref ent.Comp.ActionEntity, ent.Comp.Action);
+    }
+
+    private void OnInject(Entity<SelfInjectComponent> ent, ref ActionSelfInjectEvent args)
+    {
+        if (!TryComp<BloodstreamComponent>(ent, out var bloodstream))
+            return;
+
+        if (bloodstream.BloodSolution is not { } bloodSolution)
+            return;
+
+        if (!_solution.TryAddReagent(bloodSolution, ent.Comp.Reagent, ent.Comp.Amount))
+            return;
+
+        _audio.PlayPredicted(ent.Comp.InjectSound, ent, args.Performer);
+
+        args.Handled = true;
+    }
+}
---
+++ Content.Shared/_Floof/SelfInject/Component/SelfInjectComponent.cs
@@ -0,0 +1,42 @@
+﻿using Content.Shared.Actions;
+using Content.Shared.FixedPoint;
+using Robust.Shared.Audio;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._Floof.SelfInject.Component;
+
+[RegisterComponent]
+public sealed partial class SelfInjectComponent : Robust.Shared.GameObjects.Component
+{
+    /// <summary>
+    /// Action to create.
+    /// </summary>
+    [DataField(required: true)]
+    public EntProtoId Action = string.Empty;
+
+    [DataField]
+    public EntityUid? ActionEntity;
+
+    /// <summary>
+    /// Reagent to inject.
+    /// </summary>
+    [DataField(required: true)]
+    public string Reagent;
+
+    /// <summary>
+    /// Amount of reagent to inject.
+    /// </summary>
+    [DataField]
+    public FixedPoint2 Amount = 5;
+
+    /// <summary>
+    /// Sound to play when we inject.
+    /// </summary>
+    [DataField("injectSound")]
+    public SoundSpecifier InjectSound = new SoundPathSpecifier("/Audio/Items/hypospray.ogg");
+}
+
+/// <summary>
+/// Action event for injecting the chosen reagent.
+/// </summary>
+public sealed partial class ActionSelfInjectEvent : InstantActionEvent;
