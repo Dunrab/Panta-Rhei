76be3d65de2bbdd4cdcda7b90794419871d7f38d
Dunrab <dunrab@gmail.com>
2026-08-25 09:46:35 -0400

    requested change

--- Content.Shared/_Floof/Smoking/MessySmokerSystem.cs
+++ Content.Shared/_Floof/Smoking/MessySmokerSystem.cs
@@ -3,6 +3,7 @@ using Content.Shared._Floof.Smoking.Event;
 using Content.Shared.Fluids;
 using Content.Shared.Popups;
 using Content.Shared.Random.Helpers;
+using Robust.Shared.Network;
 using Robust.Shared.Random;
 using Robust.Shared.Timing;
 
@@ -13,6 +14,7 @@ public sealed class MessySmokerSystem : EntitySystem
     [Dependency] private readonly SharedPuddleSystem _puddle = default!;
     [Dependency] private readonly SharedPopupSystem _popup = default!;
     [Dependency] private readonly IGameTiming _timing = default!;
+    [Dependency] private readonly INetManager _netManager = default!;
 
     public override void Initialize() => SubscribeLocalEvent<MessySmokerComponent, SmokingEvent>(OnSmoked);
 
@@ -22,6 +24,9 @@ public sealed class MessySmokerSystem : EntitySystem
         var seed = SharedRandomExtensions.HashCodeCombine((int)_timing.CurTick.Value, GetNetEntity(ent).Id);
         var rand = new System.Random(seed);
         if (!rand.Prob(ent.Comp.SpitChance))
+            return;
+
+        if (_netManager.IsClient)
             return;
 
         if (ent.Comp.SpitMessagePopup != null)
